755a16114cb1448350b3f893ec0b52259fb72e19
SunXP <SunXP@users.noreply.github.com>
2017-07-17 19:27:38 -0400

    removed the hard-coded colors to randomize them
    also added background color when choosing right color

--- colorgame.js
+++ colorgame.js
@@ -1,16 +1,10 @@
-var colors=[
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0 , 255)"
-]
+var colors= generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -22,6 +16,7 @@ for (var i=0; i < squares.length; i++){
 		
 		if (clickedColor === pickedColor){
 			messageDisplay.textContent = "Correct!";
+			h1.style.backgroundColor = clickedColor;
 			changeColors(clickedColor);
 		}
 		else{
@@ -40,4 +35,19 @@ function changeColors(color){
 function pickColor(){
 	var random = Math.floor(Math.random() * colors.length);
 	return colors[random];
+}
+
+function generateRandomColors(nums){
+	var arr = [];
+	for (var i=0; i < nums; i++){
+		arr.push(randomColor());
+	}
+	return arr;
+}
+
+function randomColor(){
+	var r = Math.floor(Math.random() * 256);
+	var g = Math.floor(Math.random() * 256);
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r + ", " + g + ", " +b + ")";
 }
