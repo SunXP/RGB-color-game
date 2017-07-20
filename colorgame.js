9577a8175d88028711791cfde5245bdb8f7ac04b
SunXP <SunXP@users.noreply.github.com>
2017-07-20 17:57:12 -0400

    added an easy mode + refactored some code

--- colorgame.js
+++ colorgame.js
@@ -1,14 +1,50 @@
-var colors= generateRandomColors(6);
+var numSquares = 6;
 
+var colors= generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+easyBtn.addEventListener("click", function(){
+	resetButton.textContent="New Colors";
+	easyBtn.classList.add("selected");
+	hardBtn.classList.remove("selected");
+	numSquares=6;
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for (var i=0; i<squares.length; i++){
+		if(colors[i]){
+			squares[i].style.background = colors[i];
+		}
+		else {
+			squares[i].style.display = "none";
+		}
+	}
+});
+
+hardBtn.addEventListener("click", function(){
+	resetButton.textContent="New Colors";
+	hardBtn.classList.add("selected");
+	easyBtn.classList.remove("selected");
+	numSquares =6;
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for (var i=0; i<squares.length; i++){
+		squares[i].style.background = colors[i];
+		squares[i].style.display = "block";
+		}
+});
 
 resetButton.addEventListener("click", function(){
-	colors= generateRandomColors(6);
+	resetButton.textContent="New Colors";
+	colors= generateRandomColors(numSquares);
 	pickedColor = pickColor();
 	colorDisplay.textContent = pickedColor;
 	
@@ -19,21 +55,24 @@ resetButton.addEventListener("click", function(){
 
 colorDisplay.textContent = pickedColor;
 
+
 for (var i=0; i < squares.length; i++){
 	squares[i].style.backgroundColor = colors[i];
-	
+		
 	squares[i].addEventListener("click", function(){
 		var clickedColor= this.style.backgroundColor;
-		
+			
 		if (clickedColor === pickedColor){
 			messageDisplay.textContent = "Correct!";
 			h1.style.backgroundColor = clickedColor;
 			changeColors(clickedColor);
+			resetButton.textContent = "Play again";
 		}
 		else{
 			this.style.backgroundColor = "#232323";
 			messageDisplay.textContent = "Try Again";
 		}
+		
 	});
 }
 
